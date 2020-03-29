6b8bd95d4be5612064f3d05ec76eaf145a3392f1
Firoz kumar <56572016+Firoz-kumar-3@users.noreply.github.com>
2020-03-29 14:47:30 +0530

    Create test_case2.c

---
+++ test_case2.c
@@ -0,0 +1,49 @@
+
+/*            --------------------- ----------------------------**test case 1**-------------------------------------------------
+Enter the no. of process you want to enter : 5
+Enter details of process[1]
+Arrival Time: 1  Burst Time: 4  Priority(1 to 15): 5   
+Enter details of process[2]
+Arrival Time: 2  Burst Time: 6  Priority(1 to 15): 7   
+Enter details of process[3]
+Arrival Time: 4  Burst Time: 2  Priority(1 to 15): 8
+Enter details of process[4]
+Arrival Time: 4  Burst Time: 7  Priority(1 to 15): 9
+Enter details of process[5]
+Arrival Time: 4  Burst Time: 6  Priority(1 to 15): 14
+
+Process[1] belongs to Queue 1
+Process[2] belongs to Queue 2
+Process[3] belongs to Queue 2
+Process[4] belongs to Queue 2
+Process[5] belongs to Queue 3
+Time Quantum between the 3 queues is 10
+
+Queue1 is running for 4 units
+Queue2 is running for 10 units
+Queue3 is running for 6 units
+Queue2 is running for 5 units
+Time Quantum for Queue1 is 4
+Process ID	Burst Time	 Turnaround Time	 Waiting Time
+
+Process[1] of Queue1 is running for 4 units
+Process[1]	  4	              3	                     0
+Average Waiting Time:   0.000000
+Average Turnaround Time:3.000000
+
+Process		Burst Time	Waiting Time	Turnaround Time
+Process[1]	  6		   0		    6
+Average Waiting Time=0.000000
+Average Turnaround Time=6.000000
+Process[1] of Queue2 is running for 6 units
+
+Process ID	    Burst Time    Waiting Time	  Turnaround Time
+Process[1]		 6		 0		 6
+Process[2]		 2		 2		 4
+Process[3]		 7		 4		 11
+
+Average Waiting Time:	2.000000
+Average Turnaround Time:	7.000000
+Process[1] of Queue2 is running for 6 units
+Process[2] of Queue2 is running for 2 units
+Process[3] of Queue2 is running for 7 units
