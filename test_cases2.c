8e5b6fedccd7a166a31d52fb0e6aee57fcae011c
Firoz kumar <56572016+Firoz-kumar-3@users.noreply.github.com>
2020-03-29 14:45:52 +0530

    Create test_cases2.c

---
+++ test_cases2.c
@@ -0,0 +1,50 @@
+Enter the no. of process you want to enter : 3
+
+Enter details of process[1]
+
+Arrival Time:2 Burst Time:  3Priority(1 to 15):4
+
+Enter details of process[2]
+
+Arrival Time:3 Burst Time: 4 Priority(1 to 15): 7
+
+Enter details of process[3]
+
+Arrival Time: 4 Burst Time: 8 Priority(1 to 15): 13 
+Process[1] belongs to Queue 1
+Process[2] belongs to Queue 2
+Process[3] belongs to Queue 3
+
+
+Time Quantum between the 3 queues is 10
+Queue1 is running for 3 units
+Queue2 is running for 4 units
+Queue3 is running for 8 units
+Time Quantum for Queue1 is 4
+
+Process ID	Burst Time	 Turnaround Time	 Waiting Time
+
+Process[1] of Queue1 is running for 3 units
+Process[1]	3	1	-2
+Average Waiting Time:-2.000000
+Average Turnaround Time:1.000000
+
+Process		Burst Time	Waiting Time	Turnaround Time
+
+Process[1]		8		0		8
+
+Average Waiting Time=0.000000
+Average Turnaround Time=8.000000
+Process[1] of Queue2 is running for 8 units
+Process ID		Burst Time	 Waiting Time	 Turnaround Time
+
+Process[1]		4		 0		 4
+
+Average Waiting Time:	0.000000
+Average Turnaround Time:	4.000000
+
+Process[1] of Queue2 is running for 4 units
+
+
+
+
